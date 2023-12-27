9644e4832abb00564fd76991fa01dfbb20314b86
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-12-27 21:15:45 +0100

    application structure is specified

--- discovery-server/src/main/java/com/ensmicro/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/ensmicro/discoveryserver/DiscoveryServerApplication.java
@@ -1,12 +1,12 @@
-package com.ensmicro;
+package com.ensmicro.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryServerApplication {
+@SpringBootApplication
+class DiscoveryServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
